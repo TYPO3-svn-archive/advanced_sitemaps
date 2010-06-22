3ee206c43f3f3f961ca221653d3ad23d66e53e40
sebastiaandej <sebastiaandej@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-06-22 11:45:52 +0000

    * Added option to exclude pages from the plugin * Added static template inclusion (ext_tables.php)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/advanced_sitemaps/trunk@34725 735d13b6-9817-0410-8766-e36946ffe9aa

--- advanced_sitemaps/ext_emconf.php
+++ advanced_sitemaps/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "advanced_sitemaps".
 #
-# Auto generated 22-06-2010 11:46
+# Auto generated 22-06-2010 13:43
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"b525";s:10:"README.txt";s:4:"f2c7";s:4:"ToDo";s:4:"76e5";s:12:"ext_icon.gif";s:4:"3d26";s:17:"ext_localconf.php";s:4:"6950";s:14:"ext_tables.php";s:4:"8ea3";s:14:"ext_tables.sql";s:4:"a7dd";s:43:"icon_tx_advancedsitemaps_configurations.gif";s:4:"81b6";s:46:"icon_tx_advancedsitemaps_configurations__h.gif";s:4:"95a8";s:16:"locallang_db.xml";s:4:"e1b1";s:7:"tca.php";s:4:"4604";s:14:"doc/manual.sxw";s:4:"0810";s:37:"pi1/class.tx_advancedsitemaps_pi1.php";s:4:"082a";s:34:"pi1/ds.tx_advancedsitemaps_pi1.xml";s:4:"fb28";s:25:"pi1/locallang_csh_pi1.xml";s:4:"8a00";s:23:"res/template_google.xml";s:4:"7e17";s:22:"res/template_html.html";s:4:"b05f";s:20:"res/template_rss.xml";s:4:"59f4";s:16:"static/setup.txt";s:4:"89ec";s:39:"view/class.tx_advancedsitemaps_view.php";s:4:"c690";}',
+	'_md5_values_when_last_written' => 'a:19:{s:9:"ChangeLog";s:4:"147b";s:10:"README.txt";s:4:"f2c7";s:4:"ToDo";s:4:"76e5";s:12:"ext_icon.gif";s:4:"3d26";s:17:"ext_localconf.php";s:4:"6950";s:14:"ext_tables.php";s:4:"8940";s:14:"ext_tables.sql";s:4:"a7dd";s:43:"icon_tx_advancedsitemaps_configurations.gif";s:4:"81b6";s:46:"icon_tx_advancedsitemaps_configurations__h.gif";s:4:"95a8";s:16:"locallang_db.xml";s:4:"6b06";s:7:"tca.php";s:4:"4604";s:14:"doc/manual.sxw";s:4:"0810";s:37:"pi1/class.tx_advancedsitemaps_pi1.php";s:4:"97cb";s:34:"pi1/ds.tx_advancedsitemaps_pi1.xml";s:4:"7803";s:25:"pi1/locallang_csh_pi1.xml";s:4:"efba";s:23:"res/template_google.xml";s:4:"7e17";s:22:"res/template_html.html";s:4:"c56e";s:16:"static/setup.txt";s:4:"89ec";s:39:"view/class.tx_advancedsitemaps_view.php";s:4:"c690";}',
 	'suggests' => array(
 	),
 );
