46c913c76f79d557ea809ebea8d1f12dbc49081d
sebastiaandej <sebastiaandej@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-06-22 10:05:20 +0000

    * Initial upload of the first fully functional version. From here on changes and updates can be made.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/advanced_sitemaps/trunk@34710 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ advanced_sitemaps/ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "advanced_sitemaps".
+#
+# Auto generated 22-06-2010 11:46
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Advanced Sitemaps',
+	'description' => 'Allows you to create and customize different sitemaps for your website. Supports records (like tt_news etc.) and different output formats (HTML, XML, RSS, ATOM, Google Sitemaps)',
+	'category' => 'plugin',
+	'author' => 'Sebastiaan de Jonge',
+	'author_email' => 'szebi.eger@gmail.com',
+	'author_company' => 'SebastiaanDeJonge.com',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"b525";s:10:"README.txt";s:4:"f2c7";s:4:"ToDo";s:4:"76e5";s:12:"ext_icon.gif";s:4:"3d26";s:17:"ext_localconf.php";s:4:"6950";s:14:"ext_tables.php";s:4:"8ea3";s:14:"ext_tables.sql";s:4:"a7dd";s:43:"icon_tx_advancedsitemaps_configurations.gif";s:4:"81b6";s:46:"icon_tx_advancedsitemaps_configurations__h.gif";s:4:"95a8";s:16:"locallang_db.xml";s:4:"e1b1";s:7:"tca.php";s:4:"4604";s:14:"doc/manual.sxw";s:4:"0810";s:37:"pi1/class.tx_advancedsitemaps_pi1.php";s:4:"082a";s:34:"pi1/ds.tx_advancedsitemaps_pi1.xml";s:4:"fb28";s:25:"pi1/locallang_csh_pi1.xml";s:4:"8a00";s:23:"res/template_google.xml";s:4:"7e17";s:22:"res/template_html.html";s:4:"b05f";s:20:"res/template_rss.xml";s:4:"59f4";s:16:"static/setup.txt";s:4:"89ec";s:39:"view/class.tx_advancedsitemaps_view.php";s:4:"c690";}',
+	'suggests' => array(
+	),
+);
+
+?>
---
+++ advanced_sitemaps/ext_tables.php
@@ -0,0 +1,46 @@
+<?php
+if (!defined ('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+t3lib_extMgm::allowTableOnStandardPages('tx_advancedsitemaps_configurations');
+
+$TCA['tx_advancedsitemaps_configurations'] = array (
+	'ctrl' => array (
+		'title' => 'LLL:EXT:advanced_sitemaps/locallang_db.xml:tx_advancedsitemaps_configurations',		
+		'label' => 'tablename',
+		'label_userFunc' => 'tx_advancedsitemaps_tca->labelFunc',
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'languageField' => 'sys_language_uid',	
+		'transOrigPointerField' => 'l10n_parent',	
+		'transOrigDiffSourceField' => 'l10n_diffsource',	
+		'sortby' => 'sorting',	
+		'delete' => 'deleted',
+		'dividers2tabs' => TRUE,
+		'enablecolumns' => array (		
+			'disabled' => 'hidden',	
+			'starttime' => 'starttime',	
+			'endtime' => 'endtime',	
+			'fe_group' => 'fe_group',
+		),
+		'requestUpdate' => 'tablename',
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca.php',
+		'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY).'icon_tx_advancedsitemaps_configurations.gif',
+	),
+);
+
+t3lib_div::loadTCA('tt_content');
+t3lib_div::loadTCA('tx_advancedsitemaps_configurations');
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1']='pi_flexform';
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key';
+
+t3lib_extMgm::addPlugin(array(
+	'LLL:EXT:advanced_sitemaps/locallang_db.xml:tt_content.list_type_pi1',
+	$_EXTKEY . '_pi1',
+	t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
+),'list_type');
+
+t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY.'/pi1/ds.tx_advancedsitemaps_pi1.xml','list');
+?>
