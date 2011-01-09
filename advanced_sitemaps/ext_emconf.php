c638bbc6d3b8cfe660ab818f3e1a9576bbe1b1a4
sebastiaandej <sebastiaandej@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-01-09 11:28:53 +0000

    * Updated to version 1.1.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/advanced_sitemaps/trunk@41924 735d13b6-9817-0410-8766-e36946ffe9aa

--- advanced_sitemaps/ext_emconf.php
+++ advanced_sitemaps/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "advanced_sitemaps".
 #
-# Auto generated 04-10-2010 22:17
+# Auto generated 09-01-2011 12:27
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '1.0.0',
+	'version' => '1.1.0',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:21:{s:9:"ChangeLog";s:4:"9a57";s:10:"README.txt";s:4:"f2c7";s:4:"ToDo";s:4:"76e5";s:50:"class.tx_advancedsitemaps_recordConfigurations.php";s:4:"fcf2";s:21:"ext_conf_template.txt";s:4:"df8a";s:12:"ext_icon.gif";s:4:"3d26";s:17:"ext_localconf.php";s:4:"5d27";s:14:"ext_tables.php";s:4:"4c78";s:14:"ext_tables.sql";s:4:"9324";s:43:"icon_tx_advancedsitemaps_configurations.gif";s:4:"81b6";s:46:"icon_tx_advancedsitemaps_configurations__h.gif";s:4:"95a8";s:16:"locallang_db.xml";s:4:"645c";s:7:"tca.php";s:4:"4604";s:14:"doc/manual.sxw";s:4:"8a01";s:37:"pi1/class.tx_advancedsitemaps_pi1.php";s:4:"04f3";s:34:"pi1/ds.tx_advancedsitemaps_pi1.xml";s:4:"465f";s:25:"pi1/locallang_csh_pi1.xml";s:4:"efba";s:23:"res/template_google.xml";s:4:"7e17";s:22:"res/template_html.html";s:4:"c56e";s:16:"static/setup.txt";s:4:"89ec";s:39:"view/class.tx_advancedsitemaps_view.php";s:4:"8124";}',
+	'_md5_values_when_last_written' => 'a:22:{s:9:"ChangeLog";s:4:"4721";s:10:"README.txt";s:4:"f2c7";s:4:"ToDo";s:4:"76e5";s:50:"class.tx_advancedsitemaps_recordConfigurations.php";s:4:"a765";s:21:"ext_conf_template.txt";s:4:"df8a";s:12:"ext_icon.gif";s:4:"3d26";s:17:"ext_localconf.php";s:4:"5d27";s:14:"ext_tables.php";s:4:"4c78";s:14:"ext_tables.sql";s:4:"7e9b";s:43:"icon_tx_advancedsitemaps_configurations.gif";s:4:"81b6";s:46:"icon_tx_advancedsitemaps_configurations__h.gif";s:4:"95a8";s:16:"locallang_db.xml";s:4:"9951";s:7:"tca.php";s:4:"d24f";s:14:"doc/manual.sxw";s:4:"8a01";s:37:"pi1/class.tx_advancedsitemaps_pi1.php";s:4:"cff4";s:34:"pi1/ds.tx_advancedsitemaps_pi1.xml";s:4:"20ff";s:25:"pi1/locallang_csh_pi1.xml";s:4:"efba";s:23:"res/template_google.xml";s:4:"7e17";s:28:"res/template_google_news.xml";s:4:"1cbb";s:22:"res/template_html.html";s:4:"c56e";s:16:"static/setup.txt";s:4:"83f4";s:39:"view/class.tx_advancedsitemaps_view.php";s:4:"15b6";}',
 	'suggests' => array(
 	),
 );
