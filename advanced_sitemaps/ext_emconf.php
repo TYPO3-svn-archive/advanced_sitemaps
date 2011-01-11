23cef859dd841dbcb1c6063646878023257d6855
sebastiaandej <sebastiaandej@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-01-11 10:28:29 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/advanced_sitemaps/tags/1.1.1@41992 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ advanced_sitemaps/ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "advanced_sitemaps".
+#
+# Auto generated 11-01-2011 11:08
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Advanced Sitemaps',
+	'description' => 'Allows you to create and customize different sitemaps for your website. Supports records (like tt_news etc.) and different output formats (HTML, XML, RSS, ATOM, Google Sitemaps)',
+	'category' => 'plugin',
+	'author' => 'Sebastiaan de Jonge',
+	'author_email' => 'szebi.eger@gmail.com',
+	'author_company' => 'SebastiaanDeJonge.com',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'version' => '1.1.1',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:23:{s:9:"ChangeLog";s:4:"0f19";s:10:"README.txt";s:4:"f2c7";s:4:"ToDo";s:4:"76e5";s:17:"advanced_sitemaps";s:4:"d41d";s:50:"class.tx_advancedsitemaps_recordConfigurations.php";s:4:"a765";s:21:"ext_conf_template.txt";s:4:"df8a";s:12:"ext_icon.gif";s:4:"3d26";s:17:"ext_localconf.php";s:4:"5d27";s:14:"ext_tables.php";s:4:"4c78";s:14:"ext_tables.sql";s:4:"f8ac";s:43:"icon_tx_advancedsitemaps_configurations.gif";s:4:"81b6";s:46:"icon_tx_advancedsitemaps_configurations__h.gif";s:4:"95a8";s:16:"locallang_db.xml";s:4:"9951";s:7:"tca.php";s:4:"d24f";s:14:"doc/manual.sxw";s:4:"cc3d";s:37:"pi1/class.tx_advancedsitemaps_pi1.php";s:4:"cff4";s:34:"pi1/ds.tx_advancedsitemaps_pi1.xml";s:4:"20ff";s:25:"pi1/locallang_csh_pi1.xml";s:4:"efba";s:23:"res/template_google.xml";s:4:"7e17";s:28:"res/template_google_news.xml";s:4:"1cbb";s:22:"res/template_html.html";s:4:"c56e";s:16:"static/setup.txt";s:4:"83f4";s:39:"view/class.tx_advancedsitemaps_view.php";s:4:"15b6";}',
+	'suggests' => array(
+	),
+);
+
+?>
---
+++ advanced_sitemaps/ext_tables.sql
@@ -0,0 +1,53 @@
+#
+# Table structure for table 'tx_advancedsitemaps_configurations'
+#
+CREATE TABLE tx_advancedsitemaps_configurations (
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+	tstamp int(11) DEFAULT '0' NOT NULL,
+	crdate int(11) DEFAULT '0' NOT NULL,
+	cruser_id int(11) DEFAULT '0' NOT NULL,
+	sys_language_uid int(11) DEFAULT '0' NOT NULL,
+	l10n_parent int(11) DEFAULT '0' NOT NULL,
+	l10n_diffsource mediumtext,
+	sorting int(10) DEFAULT '0' NOT NULL,
+	deleted tinyint(4) DEFAULT '0' NOT NULL,
+	hidden tinyint(4) DEFAULT '0' NOT NULL,
+	starttime int(11) DEFAULT '0' NOT NULL,
+	endtime int(11) DEFAULT '0' NOT NULL,
+	fe_group int(11) DEFAULT '0' NOT NULL,
+	tablename tinytext,
+	parent tinytext,
+	url_params tinytext,
+	sortby tinytext,
+	sortby_dir tinytext,
+	record_limit int(11) DEFAULT '0' NOT NULL,
+	title_field tinytext,
+	single_page text,
+	startingpoint text,
+	recursive tinytext,
+	andWhere text,
+	gs_template tinytext,
+	gs_priority char(3) DEFAULT '' NOT NULL,
+	gs_changeFreq varchar(16) DEFAULT '' NOT NULL,
+	gsn_dateField varchar(255) DEFAULT '' NOT NULL,
+	gsn_keywordsField varchar(255) DEFAuLT '' NOT NULL,
+	gsn_stockTickerField varchar(255) DEFAuLT '' NOT NULL,
+	
+	PRIMARY KEY (uid),
+	KEY parent (pid)
+);
+
+CREATE TABLE pages (
+	tx_advancedsitemaps_priority char(3) DEFAULT '' NOT NULL,
+	tx_advancedsitemaps_changeFreq varchar(16) DEFAULT '' NOT NULL
+);
+
+CREATE TABLE tx_advancedsitemaps_configurations_records (
+	`table_name` varchar(255) DEFAULT '' NOT NULL,
+	`record_uid` int(11) DEFAULT '0' NOT NULL,
+	`dataType` varchar(24) DEFAULT '' NOT NULL,
+	`value` varchar(16) DEFAULT '' NOT NULL,
+	
+	PRIMARY KEY (table_name,record_uid,dataType)
+);
