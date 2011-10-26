7857bf7639011c75d2451435cedc01800b234b72
sebastiaandej <sebastiaandej@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-10-26 10:40:54 +0000

    Updated version to 1.1.3
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/advanced_sitemaps/trunk@53443 735d13b6-9817-0410-8766-e36946ffe9aa

--- advanced_sitemaps/ext_emconf.php
+++ advanced_sitemaps/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "advanced_sitemaps".
 #
-# Auto generated 11-02-2011 10:56
+# Auto generated 26-10-2011 10:17
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -11,36 +11,36 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Advanced Sitemaps',
-	'description' => 'Allows you to create and customize different sitemaps for your website. Supports records (like tt_news etc.) and different output formats (HTML, XML, RSS, ATOM, Google Sitemaps)',
-	'category' => 'plugin',
-	'author' => 'Sebastiaan de Jonge',
-	'author_email' => 'szebi.eger@gmail.com',
-	'author_company' => 'SebastiaanDeJonge.com',
-	'shy' => '',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'module' => '',
-	'state' => 'beta',
-	'internal' => '',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 1,
-	'lockType' => '',
-	'version' => '1.1.2',
-	'constraints' => array(
-		'depends' => array(
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'_md5_values_when_last_written' => 'a:22:{s:9:"ChangeLog";s:4:"c166";s:10:"README.txt";s:4:"f2c7";s:4:"ToDo";s:4:"76e5";s:50:"class.tx_advancedsitemaps_recordConfigurations.php";s:4:"9529";s:21:"ext_conf_template.txt";s:4:"df8a";s:12:"ext_icon.gif";s:4:"3d26";s:17:"ext_localconf.php";s:4:"5d27";s:14:"ext_tables.php";s:4:"4c78";s:14:"ext_tables.sql";s:4:"7cc5";s:43:"icon_tx_advancedsitemaps_configurations.gif";s:4:"81b6";s:46:"icon_tx_advancedsitemaps_configurations__h.gif";s:4:"95a8";s:16:"locallang_db.xml";s:4:"9951";s:7:"tca.php";s:4:"d24f";s:14:"doc/manual.sxw";s:4:"cc3d";s:37:"pi1/class.tx_advancedsitemaps_pi1.php";s:4:"27ac";s:34:"pi1/ds.tx_advancedsitemaps_pi1.xml";s:4:"20ff";s:25:"pi1/locallang_csh_pi1.xml";s:4:"efba";s:23:"res/template_google.xml";s:4:"7e17";s:28:"res/template_google_news.xml";s:4:"1cbb";s:22:"res/template_html.html";s:4:"c56e";s:16:"static/setup.txt";s:4:"83f4";s:39:"view/class.tx_advancedsitemaps_view.php";s:4:"15b6";}',
-	'suggests' => array(
-	),
+    'title' => 'Advanced Sitemaps',
+    'description' => 'Allows you to create and customize different sitemaps for your website. Supports records (like tt_news etc.) and different output formats (HTML, XML, RSS, ATOM, Google Sitemaps)',
+    'category' => 'plugin',
+    'author' => 'Sebastiaan de Jonge',
+    'author_email' => 'szebi.eger@gmail.com',
+    'author_company' => 'SebastiaanDeJonge.com',
+    'shy' => '',
+    'dependencies' => '',
+    'conflicts' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'beta',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 1,
+    'lockType' => '',
+    'version' => '1.1.3',
+    'constraints' => array(
+        'depends' => array(
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+    '_md5_values_when_last_written' => 'a:23:{s:9:"ChangeLog";s:4:"954d";s:10:"README.txt";s:4:"f2c7";s:4:"ToDo";s:4:"76e5";s:50:"class.tx_advancedsitemaps_recordConfigurations.php";s:4:"9529";s:33:"class.tx_advancedsitemaps_tca.php";s:4:"d6fc";s:21:"ext_conf_template.txt";s:4:"df8a";s:12:"ext_icon.gif";s:4:"3d26";s:17:"ext_localconf.php";s:4:"5d27";s:14:"ext_tables.php";s:4:"5f24";s:14:"ext_tables.sql";s:4:"7a7f";s:43:"icon_tx_advancedsitemaps_configurations.gif";s:4:"81b6";s:46:"icon_tx_advancedsitemaps_configurations__h.gif";s:4:"95a8";s:16:"locallang_db.xml";s:4:"10cf";s:7:"tca.php";s:4:"2a86";s:14:"doc/manual.sxw";s:4:"cc3d";s:37:"pi1/class.tx_advancedsitemaps_pi1.php";s:4:"2e92";s:34:"pi1/ds.tx_advancedsitemaps_pi1.xml";s:4:"20ff";s:25:"pi1/locallang_csh_pi1.xml";s:4:"efba";s:23:"res/template_google.xml";s:4:"7e17";s:28:"res/template_google_news.xml";s:4:"1cbb";s:22:"res/template_html.html";s:4:"c56e";s:16:"static/setup.txt";s:4:"2ea2";s:39:"view/class.tx_advancedsitemaps_view.php";s:4:"15b6";}',
+    'suggests' => array(
+    ),
 );
 
 ?>
